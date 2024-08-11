0656c32521f0de842bd430144cb42beef190dc4b
GronyX432 <buksi690@gmail.com>
2024-08-11 10:05:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Játékban: MineElysium","Hallgatja: MineElysium"];
 
 
 let currentIndex = 0;
